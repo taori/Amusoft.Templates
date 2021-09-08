93e3be7a201a1905a12a23fbe3df638aa6c23fec
Andreas Müller <taori@users.noreply.github.com>
2021-09-08 15:35:25 +0200

    basic scripts + project structure

---
+++ scripts/buildpackage.ps1
@@ -0,0 +1,11 @@
+dotnet build ..\src\All.sln -v q
+
+Write-Host "Removing artifacts folder ..."
+Remove-Item -Recurse -Force -Path ..\artifacts -ErrorAction SilentlyContinue
+Write-Host "done." -ForegroundColor Green
+
+Write-Host "Creating artifacts folder structure ..."
+New-Item ..\artifacts -ItemType Directory | Out-Null
+Write-Host "done." -ForegroundColor Green
+
+dotnet pack ..\src\Amusoft.Templates\Amusoft.Templates.csproj -o ..\artifacts -v m
