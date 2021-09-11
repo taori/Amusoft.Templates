33d31e373626ac6b71de64ebb00e55fdc2fcd89f
Andreas Müller <taori@users.noreply.github.com>
2021-09-11 23:38:56 +0200

    current template update progress

---
+++ src/Amusoft.Templates/templates/dotnet-library-repo/scripts/build.ps1
@@ -0,0 +1,13 @@
+$configuration = "Release"
+$verbosity = "n"
+
+dotnet restore ../src/All.sln --verbosity $verbosity
+Write-Host "Restore complete" -ForegroundColor Green
+
+dotnet build ../src/All.sln --verbosity $verbosity -c $configuration --no-restore
+Write-Host "Build complete" -ForegroundColor Green
+
+dotnet test ../src/All.sln --verbosity $verbosity -c $configuration --no-build 
+Write-Host "Test complete" -ForegroundColor Green
+
+dotnet pack ../src/MyLibrary/MyLibrary.csproj --verbosity $verbosity -c $configuration -o ../artifacts/nupkg --no-build /p:VersionSuffix=$versionSuffix
--- src/Amusoft.Templates/templates/dotnet-library-repo/tests/MyLibrary.UnitTests/UnitTest1.cs
+++ src/Amusoft.Templates/templates/dotnet-library-repo/tests/MyLibrary.UnitTests/UnitTest1.cs
@@ -1,13 +1,19 @@
+using Amusoft.XUnit.NLog.Extensions;
+using MyLibrary.UnitTests.Toolkit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MyLibrary.UnitTests
 {
-    public class UnitTest1
+    public class UnitTest1 : TestBase
     {
         [Fact]
         public void Test1()
         {
+        }
 
+        public UnitTest1(ITestOutputHelper outputHelper, GlobalSetupFixture data) : base(outputHelper, data)
+        {
         }
     }
 }
