6adb550f686019519980645e7028796775297150
Andreas Müller <taori@users.noreply.github.com>
2022-10-06 11:51:29 +0200

    Update build.ps1

--- src/Amusoft.Templates/templates/dotnet-library-repo/scripts/build.ps1
+++ src/Amusoft.Templates/templates/dotnet-library-repo/scripts/build.ps1
@@ -1,6 +1,11 @@
 $configuration = "Release"
-$verbosity = "n"
+$verbosity = "m"
 $versionSuffix = "beta"
+$runTest = $false
+$runPack = $true
+
+$dateFormat = [System.DateTime]::Now.ToString("yyyyMMdd.HHmm")
+$versionSuffix = "$($versionSuffix).$($dateFormat)"
 
 dotnet restore "$PSScriptRoot/../src/All.sln" --verbosity $verbosity
 Write-Host "Restore complete" -ForegroundColor Green
@@ -8,7 +13,11 @@ Write-Host "Restore complete" -ForegroundColor Green
 dotnet build "$PSScriptRoot/../src/All.sln" --verbosity $verbosity -c $configuration --no-restore
 Write-Host "Build complete" -ForegroundColor Green
 
-dotnet test "$PSScriptRoot/../src/All.sln" --verbosity $verbosity -c $configuration --no-build 
-Write-Host "Test complete" -ForegroundColor Green
+if($runTest){
+  dotnet test "$PSScriptRoot/../src/All.sln" --verbosity $verbosity -c $configuration --no-build 
+  Write-Host "Test complete" -ForegroundColor Green
+}
 
-dotnet pack "$PSScriptRoot/../src/MyLibrary/MyLibrary.csproj" --verbosity $verbosity -c $configuration -o ../artifacts/nupkg --no-build /p:VersionSuffix=$versionSuffix
+if($runPack){
+  dotnet pack "$PSScriptRoot/../src/MyLibrary/MyLibrary.csproj" --verbosity $verbosity -c $configuration -o ../artifacts/nupkg --no-build /p:VersionSuffix=$versionSuffix
+}
