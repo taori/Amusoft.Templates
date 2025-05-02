3255f6870d59f9a6c1bba87eec58b8b422e68049
Andreas Müller <taori@users.noreply.github.com>
2025-05-02 02:43:39 +0200

    global tool update

---
+++ src/Amusoft.Templates/templates/dotnet-global-tool/src/MyConsoleApp/Integration/DependencyInjection.cs
@@ -0,0 +1,60 @@
+﻿// This file is licensed to you under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Spectre.Console.Cli;
+
+namespace MyConsoleApp.Integration;
+
+internal sealed class TypeRegistrar : ITypeRegistrar
+{
+	private readonly IHostBuilder _builder;
+
+	public TypeRegistrar(IHostBuilder builder)
+	{
+		_builder = builder;
+	}
+
+	public ITypeResolver Build()
+	{
+		return new TypeResolver(_builder.Build());
+	}
+
+	public void Register(Type service, Type implementation)
+	{
+		_builder.ConfigureServices((_, services) => services.AddSingleton(service, implementation));
+	}
+
+	public void RegisterInstance(Type service, object implementation)
+	{
+		_builder.ConfigureServices((_, services) => services.AddSingleton(service, implementation));
+	}
+
+	public void RegisterLazy(Type service, Func<object> func)
+	{
+		if (func is null) throw new ArgumentNullException(nameof(func));
+
+		_builder.ConfigureServices((_, services) => services.AddSingleton(service, _ => func()));
+	}
+}
+
+internal sealed class TypeResolver : ITypeResolver, IDisposable
+{
+	private readonly IHost _host;
+
+	public TypeResolver(IHost provider)
+	{
+		_host = provider ?? throw new ArgumentNullException(nameof(provider));
+	}
+
+	public object? Resolve(Type? type)
+	{
+		return type != null ? _host.Services.GetService(type) : null;
+	}
+
+	public void Dispose()
+	{
+		_host.Dispose();
+	}
+}
--- src/Amusoft.Templates/templates/dotnet-global-tool/src/MyConsoleApp/Program.cs
+++ src/Amusoft.Templates/templates/dotnet-global-tool/src/MyConsoleApp/Program.cs
@@ -1,8 +1,77 @@
-﻿namespace MyConsoleApp;
+﻿using Microsoft.Extensions.Hosting;
+
+using Spectre.Console.Cli;
+
+using MyConsoleApp.Integration;
+
+namespace MyConsoleApp;
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	static async Task<int> Main(string[] args)
+	{
+#if DEBUG
+		while (Console.ReadLine() is {Length: > 0} line)
+		{
+			var r = await RunApplication(line.Split(' '));
+			Console.WriteLine($"ExitCode: {r}");
+		}
+
+		return 0;
+#else
+		return await RunApplication(args);
+#endif
+	}
+
+	private static Task<int> RunApplication(string[] args)
+	{
+		var host = CreateHostBuilder(args);
+		var registrar = new TypeRegistrar(host);
+		var app = new CommandApp(registrar);
+		app.Configure(configurator =>
+			{
+				configurator.AddBranch(
+					"project", projectCommand =>
+					{
+						projectCommand.AddCommand<AddCommand>("add");
+						projectCommand.AddCommand<RemoveCommand>("remove");
+					}
+				);
+			}
+		);
+	    
+		return app.RunAsync(args);
+	}
+
+	internal static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host
+			.CreateDefaultBuilder(args)
+			.ConfigureServices((hostContext, services) =>
+			{
+				// add service registrations here
+			});
+    
+	internal class AddCommand : Command<AddCommand.AddCommandSettings>
+	{
+		public class AddCommandSettings : CommandSettings
+		{
+		}
+	    
+		public override int Execute(CommandContext context, AddCommandSettings settings)
+		{
+			throw new NotImplementedException();
+		}
+	}
+    
+	internal class RemoveCommand : Command<RemoveCommand.RemoveCommandSettings>
+	{
+		public class RemoveCommandSettings : CommandSettings
+		{
+		}
+	    
+		public override int Execute(CommandContext context, RemoveCommandSettings settings)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
